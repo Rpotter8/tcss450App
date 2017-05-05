5730afa0ef9744b020d465fc300fea24d53f32d2
Alexander Terikov <teraliv@gmail.com>
2017-05-05 14:15:53 -0700

    camera functionality added

--- app/src/main/java/group4/tcss450/uw/edu/challengeapp/VisionActivity.java
+++ app/src/main/java/group4/tcss450/uw/edu/challengeapp/VisionActivity.java
@@ -1,14 +1,19 @@
 package group4.tcss450.uw.edu.challengeapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class VisionActivity extends AppCompatActivity {
+
+    private String mPhotoPath;  // path to photo taken in Camera Activity
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +26,21 @@ public class VisionActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //         .setAction("Action", null).show();
-                Intent Intent = new Intent(view.getContext(), TestActivity.class);
-                view.getContext().startActivity(Intent);
-
+                Intent intent = new Intent(view.getContext(), CameraActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                mPhotoPath = data.getStringExtra("result");
+                Log.v("RETURNED PHOTO PATH", mPhotoPath);
+            }
+        }
+    }
 }
